1b3acb6b506f102e5e08c6cbac00889f820d5988
fifsky <fifsky@gmail.com>
2018-08-13 14:32:17 +0800

    gracefully shutdown

--- main.go
+++ main.go
@@ -4,41 +4,36 @@ import (
 	"os"
 	"fmt"
 	"flag"
+	"log"
 	"io/ioutil"
 	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/gin-gonic/gin"
+
 	"github.com/fifsky/goblog/core"
 	"github.com/fifsky/goblog/config"
-	"github.com/fifsky/goblog/router"
 	"github.com/ilibs/gosql"
-	"github.com/ilibs/logger"
 	"github.com/fifsky/goblog/server"
 )
 
 func main() {
 	gosql.Connect(config.App.DB)
-	fmt.Println("Run Mode:", gin.Mode())
 	flag.Parse()
 	cmd := flag.Arg(0)
 	if cmd == "install" {
 		_, err := config.ImportDB()
 		if err != nil {
 			fmt.Println("Import DB Error:" + err.Error())
-			logger.Error(err)
+			log.Fatalf("import error %s", err)
 		}
 		return
 	}
 
-	serv := server.Serv()
-	//路由
-	router.Route(serv)
 	setPid(os.Getpid())
 	//定时提醒
 	go core.StartCron()
 
-	serv.Run(":8080")
+	server.Run(":8080")
 }
 
 func setPid(pid int) {
--- server/serv.go
+++ server/serv.go
@@ -1,9 +1,51 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"log"
+	"os"
+	"os/signal"
+	"context"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/fifsky/goblog/router"
+)
 
 var serv = gin.Default()
 
 func Serv() *gin.Engine {
 	return serv
+}
+
+func Run(port string) {
+	srv := &http.Server{
+		Addr:    port,
+		Handler: serv,
+	}
+
+	//router
+	router.Route(serv)
+
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	srv.Close()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
 }
