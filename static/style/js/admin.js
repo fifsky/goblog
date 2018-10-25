36f85c73a70c4044a19e9098358549426d25434a
fifsky <fifsky@gmail.com>
2018-10-25 09:39:54 +0800

    complete partial batch operation

--- static/style/js/admin.js
+++ static/style/js/admin.js
@@ -1,8 +1,20 @@
-(function($){
+(function ($) {
 
-    $('a[rel="confirmTodo"]').click(function(){
+    $('a[rel="confirmTodo"]').click(function () {
         var title = $(this).attr('title') ? $(this).attr('title') : '确认？';
         return confirm(title);
+    });
+
+    $('.all-selected').on("click", function () {
+        $('.list').find('input[name="ids"]').prop("checked", true);
+    });
+
+    $('.inverse-selected').on("click", function () {
+        var $el = $('.list').find('input[name="ids"]');
+        var $el1 = $el.filter(":checked");
+        var $el2 = $el.not(":checked");
+        $el1.prop("checked", false);
+        $el2.prop("checked", true);
     });
 
 })(jQuery);
